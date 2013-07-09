72752053b30649dab3547f516908660d65764c8c
jankuca <jan@jankuca.com>
2013-07-09 16:06:23 +0200

    add "get-closure-compiler" shadow task

--- tasks/compile.js
+++ tasks/compile.js
@@ -1,80 +1,64 @@
 var async = require('async');
-var child_process = require('child_process');
-var fs = require('fs');
 var path = require('path');
-var shell = require('shelljs');
 
 
 module.exports = function (runner, args, callback) {
-  var app_dirname = runner.getProjectDirname();
   var closure_library_dirname = runner.getClosureLibraryDirname();
-  var closure_compiler_dirname = runner.getClosureCompilerDirname();
   var output_path = runner.getOutputPath();
   var source_map_path = runner.getSourceMapPath();
 
+  var compile;
+
   async.waterfall([
     function (callback) {
+      runner.runTask('get-closure-compiler', callback);
+    },
+
+    function (compile_, callback) {
+      compile = compile_;
+
       runner.runTask('scopify', callback);
     },
 
     function (sources, callback) {
-      var local_closure_path = path.join(app_dirname, 'node_modules/.bin/closure');
-      var local_closure_exists = fs.existsSync(local_closure_path);
-      var global_closure = shell.which('closure');
+      var flags = {};
+      flags['compilation_level'] = 'ADVANCED_OPTIMIZATIONS';
+      flags['warning_level'] = 'VERBOSE';
+      flags['language_in'] = 'ECMASCRIPT5_STRICT';
+      flags['jscomp_error'] = [
+        'ambiguousFunctionDecl',
+        'checkRegExp',
+        'es5Strict',
+        'externsValidation',
+        'globalThis',
+        'missingProperties',
+        'strictModuleDepCheck',
+        'suspiciousCode',
+        'undefinedNames'
+      ];
+      flags['jscomp_warning'] = [
+        'accessControls',
+        'const',
+        'constantProperty',
+        'visibility'
+      ];
+      flags['js_output_file'] = '' + output_path;
+      flags['summary_detail_level'] = '3';
 
-      var command = '';
-      var command_args = [];
-
-      if (local_closure_exists) {
-        command = local_closure_path;
-      } else if (global_closure) {
-        command = global_closure;
-      } else {
-        command = 'java';
-        command_args.push('-jar', closure_compiler_dirname + '/compiler.jar');
-      }
-
-      command_args.push('--compilation_level=ADVANCED_OPTIMIZATIONS');
-      command_args.push('--warning_level=VERBOSE');
-      command_args.push('--language_in=ECMASCRIPT5_STRICT');
-      command_args.push('--jscomp_error=ambiguousFunctionDecl');
-      command_args.push('--jscomp_error=checkRegExp');
-      command_args.push('--jscomp_error=es5Strict');
-      command_args.push('--jscomp_error=externsValidation');
-      command_args.push('--jscomp_error=globalThis');
-      command_args.push('--jscomp_error=missingProperties');
-      command_args.push('--jscomp_error=strictModuleDepCheck');
-      command_args.push('--jscomp_error=suspiciousCode');
-      command_args.push('--jscomp_error=undefinedNames');
-      command_args.push('--jscomp_warning=accessControls');
-      command_args.push('--jscomp_warning=const');
-      command_args.push('--jscomp_warning=constantProperty');
-      command_args.push('--jscomp_warning=visibility');
-      command_args.push('--js_output_file=' + output_path);
-      command_args.push('--summary_detail_level=3');
-
-      var output_wrapper = '(function(){%output%}.call(this));';
+      flags['output_wrapper'] = '(function(){%output%}.call(this));';
       if (source_map_path) {
-        output_wrapper += ' //# sourceMappingURL=' +
+        flags['output_wrapper'] += ' //# sourceMappingURL=' +
           path.relative(path.dirname(output_path), path.dirname(source_map_path)) +
           path.basename(source_map_path);
-        command_args.push('--create_source_map=' + source_map_path);
-        command_args.push('--source_map_format=V3');
+        flags['create_source_map'] = source_map_path;
+        flags['source_map_format'] = 'V3';
       }
-      command_args.push('--output_wrapper="' + output_wrapper + '"');
+      flags['output_wrapper'] = '"' + flags['output_wrapper'] + '"';
 
-      command_args.push('--js=' + path.join(closure_library_dirname, '/closure/goog/base.js'));
-      sources.forEach(function (source) {
-        command_args.push('--js=' + source);
-      });
+      flags['js'] = sources.slice();
+      flags['js'].unshift(path.join(closure_library_dirname, '/closure/goog/base.js'));
 
-      child(command, command_args, function (err, result) {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, result.stderr);
-        }
-      })
+      compile(flags, callback);
     },
 
     function (log, callback) {
@@ -123,30 +107,3 @@ module.exports = function (runner, args, callback) {
     }
   ], callback);
 };
-
-
-function child(command, args, callback) {
-  var result = {
-    stdout: '',
-    stderr: '',
-    code: null
-  };
-
-  var proc = child_process.spawn(command, args);
-
-  proc.stdout.on('data', function (chunk) {
-    result.stdout += chunk;
-  });
-  proc.stderr.on('data', function (chunk) {
-    result.stderr += chunk;
-  });
-  proc.on('close', function (code) {
-    setTimeout(function () {
-      result.code = code || 0;
-      callback(null, result);
-    }, 0);
-  });
-  proc.on('error', function (err) {
-    callback(err, result);
-  });
-}
---
+++ tasks/get-closure-compiler.js
@@ -0,0 +1,106 @@
+var child_process = require('child_process');
+var fs = require('fs');
+var path = require('path');
+var shell = require('shelljs');
+
+
+module.exports = function (runner, args, callback) {
+  var be_verbose = args['v'];
+
+  var app_dirname = runner.getProjectDirname();
+  var closure_compiler_dirname = runner.getClosureCompilerDirname();
+
+  var local_closure_path = path.join(app_dirname, 'node_modules/.bin/closure');
+  var global_closure = shell.which('closure');
+
+  var command = '';
+  var command_args = [];
+
+  if (fs.existsSync(local_closure_path)) {
+    if (be_verbose) {
+      runner.log('Local fast-closure-compiler installation found\n');
+    }
+    command = local_closure_path;
+
+  } else if (global_closure) {
+    if (be_verbose) {
+      runner.log('Global fast-closure-compiler installation found\n');
+    }
+    command = global_closure;
+
+  } else if (shell.which('java')) {
+    if (be_verbose) {
+      runner.log('No fast-closure-compiler installation found\n');
+    }
+
+    var jar_path = closure_compiler_dirname + '/compiler.jar';
+    if (fs.existsSync(jar_path)) {
+      if (be_verbose) {
+        runner.log('Google Closure Compiler jar found\n');
+      }
+      command = 'java';
+      command_args.push('-jar', jar_path);
+    } else {
+      callback(new Error('Cannot find \033[0;31mGoogle Closure Compiler\033[0m'), null);
+      return;
+    }
+
+  } else {
+    callback(new Error('Cannot find \033[0;31mjava\033[0m'), null);
+    return;
+  }
+
+  callback(null, function (flags, callback) {
+    for (var key in flags) {
+      var value = flags[key];
+      if (Array.isArray(value)) {
+        value.forEach(function (val) {
+          command_args.push('--' + key + '=' + val);
+        });
+      } else {
+        command_args.push('--' + key + '=' + value);
+      }
+    }
+
+    if (be_verbose) {
+      runner.log('Compilation command:\n\033[2;38m');
+      runner.log(command + ' \\\n  ');
+      runner.log(command_args.join(' \\\n  ') + '\033[0m\n');
+    }
+
+    child(command, command_args, function (err, result) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, result.stderr);
+      }
+    });
+  });
+}
+
+
+function child(command, args, callback) {
+  var result = {
+    stdout: '',
+    stderr: '',
+    code: null
+  };
+
+  var proc = child_process.spawn(command, args);
+
+  proc.stdout.on('data', function (chunk) {
+    result.stdout += chunk;
+  });
+  proc.stderr.on('data', function (chunk) {
+    result.stderr += chunk;
+  });
+  proc.on('close', function (code) {
+    setTimeout(function () {
+      result.code = code || 0;
+      callback(null, result);
+    }, 0);
+  });
+  proc.on('error', function (err) {
+    callback(err, result);
+  });
+}
