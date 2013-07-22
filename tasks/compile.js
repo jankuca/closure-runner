b119804eeb00516f4b375aafe6c21468a08fe0f0
jankuca <jan@jankuca.com>
2013-07-22 15:03:58 -0700

    extract the "deps" task from the "compile" task

--- tasks/compile.js
+++ tasks/compile.js
@@ -10,11 +10,7 @@ module.exports = function (runner, args, callback) {
   var closure_template_dirname = runner.getConfigValue('closure-templates');
   var output_path = runner.getAppConfigValue('output');
   var source_map_path = runner.getAppConfigValue('output.source-map');
-  var temp_deps_path = runner.getAppConfigValue('output.deps');
   var roots = runner.getAppConfigValue('roots');
-
-  var temp_dirname = runner.getTempDirname();
-  var temp_dirname_rel = path.relative(app_dirname, temp_dirname);
 
   var scopify = (runner.getAppConfigValue('scopify') === true);
 
@@ -47,54 +43,7 @@ module.exports = function (runner, args, callback) {
       callback(null);
     },
 
-    runner.runTask.bind(runner, 'get-closure-depswriter'),
-
-    function (depswriter, callback) {
-      var flags = {};
-
-      var temp_roots = roots;
-      if (scopify) {
-        temp_roots = roots.map(function (root) {
-          var temp_root = path.join(temp_dirname_rel, root);
-          var temp_root_rel = './' + path.relative(app_dirname, temp_root);
-          return temp_root_rel;
-        });
-      }
-
-      if (!temp_roots.every(function (root) {
-        var root_closure_rel = path.relative(closure_library_dirname, root);
-        return (root_closure_rel.substr(0, 2) === '..');
-      })) {
-        temp_roots.push(closure_library_dirname);
-      }
-
-      flags['root_with_prefix'] = temp_roots.map(function (root) {
-        var root_deps_rel = path.relative(
-          './' + path.join(closure_library_dirname, '/closure/goog'),
-          root
-        );
-
-        if (!fs.existsSync(root)) {
-          mkdirp.sync(root);
-        }
-
-        return root + ' ' + root_deps_rel;
-      });
-
-      if (temp_deps_path) {
-        depswriter(flags, callback);
-      } else {
-        callback(null, '');
-      }
-    },
-
-    function (depswriter_result, callback) {
-      if (temp_deps_path) {
-        fs.writeFile(temp_deps_path, depswriter_result, callback);
-      } else {
-        callback(null);
-      }
-    },
+    runner.runTask.bind(runner, 'deps'),
 
     function (callback) {
       var app_flags = runner.getAppConfigValue('closure-compiler.flags');
