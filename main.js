9f7c16cc1e36511bbb57d9c194e6aa646b57ecb8
jankuca <jan@jankuca.com>
2013-10-10 14:57:27 +0200

    allow Closure Library usage in tasks

--- main.js
+++ main.js
@@ -15,15 +15,29 @@ function main(callback) {
   env.addConfig(path.join(env.getProjectDirname(), './client.json'));
   env.setArguments(process.argv.slice(2));
 
+  var project_dirname = env.getProjectDirname();
+
+  if (env.get('use-goog-in-tasks')) {
+    var closure_dirname_rel = env.get('closure-library');
+    var closure_dirname = path.join(project_dirname, closure_dirname_rel);
+    var goog_bootstrap_filename = path.join(
+        closure_dirname, 'closure', 'goog', 'bootstrap', 'nodejs.js');
+
+    require(goog_bootstrap_filename);
+  }
+
   async.eachSeries(env.getApps(), function (app, callback) {
     var args = env.getArguments();
     var runner = new Runner(app, args, stderr);
 
     runner.setConfig(env.getConfig());
-    runner.setProjectDirname(env.getProjectDirname());
+    runner.setProjectDirname(project_dirname);
     runner.setTasks(env.getTasks());
 
-    runner.runMainTask(env.getMainTaskId(), callback);
+    runner.runTask('deps', function () {
+      require(path.join(project_dirname, runner.getAppConfigValue('output.deps')));
+      runner.runMainTask(env.getMainTaskId(), callback);
+    });
   }, function (err) {
     if (err) {
       if (err.message.indexOf('\033[') !== -1) {
